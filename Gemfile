f283a1eacc8712ed34ed290f8d17485206c645a1
Andy Rong <andy.rong@outlook.com>
2014-01-30 15:56:39 +0800

    appfog database

--- Gemfile
+++ Gemfile
@@ -6,7 +6,7 @@ gem 'mysql2'
 #gem 'pg'
 gem 'thin'
 gem 'devise', '3.0'
-
+gem 'le'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,4 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Myapp::Application.initialize!
+Rails.logger = Le.new(ENV.fetch('LOGENTRIES_TOKEN')) if !ENV['LOGENTRIES_TOKEN'].nil?
