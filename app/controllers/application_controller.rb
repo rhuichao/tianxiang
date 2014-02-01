f782564d2c5be7cd495a6f8a80dce4d0127119e5
Andy Rong <andy.rong@outlook.com>
2014-02-01 21:12:00 +0800

    bootstrap

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,6 @@ class ApplicationController < ActionController::Base
   before_filter :authenticate_user!
 
   def after_sign_in_path_for(resource_or_scope)
-    logger.info("Hello Logentries. I'm an info message")
     "/home"
   end
 end
